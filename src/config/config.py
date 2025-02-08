f41d0429238304e773612d12ed6d674cc86b63c0
dominicdill <dill.d@northeastern.edu>
2025-02-08 16:42:13 -0500

    updated manual annotation workflow and cleaned code, added pydantic

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import DirectoryPath
+
+
+class FrameCaptureSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='src/config/.env', env_file_encoding='utf-8', extra='ignore')
+    
+    webcam_url: str
+    model_path: DirectoryPath
+    model_name: str
+    frame_interval: int
+    conf_threshold: float
+    img_dir: DirectoryPath
+    default_label_dir: DirectoryPath
+    edited_label_dir: DirectoryPath
+
+
+frame_capture_settings = FrameCaptureSettings()
