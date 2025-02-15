667e0f04883834095932dcde7df0b8a17424a018
dominicdill <dill.d@northeastern.edu>
2025-02-15 17:39:52 -0500

    adding s3 upload and download for aws ec2 instance

---
+++ src/download_s3.py
@@ -0,0 +1,20 @@
+import os
+import zipfile
+
+import boto3
+
+from ultralytics import YOLO
+
+
+bucket_name = 'dominic-dill-people-counter'
+sns_topic_arn = None
+
+
+# download data
+s3_client = boto3.client('s3')
+
+s3_client.download_file(bucket_name, 'in/datasets.zip', 'datasets.zip')
+
+with zipfile.ZipFile('datasets.zip', 'r') as zip_ref:
+    zip_ref.extractall('./')
+
